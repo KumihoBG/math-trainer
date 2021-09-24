fedb401e079d11606285e80d6beed18a055bdace
Kumiho <pavleta.taseva@gmail.com>
2021-09-24 20:49:17 +0300

    substraction added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+    // Sum numbers
     const sumOne = document.getElementById('sum-one');
     sumOne.value = Math.floor(Math.random() * 9999) + 1;
     const sumTwo = document.getElementById('sum-two');
@@ -7,8 +8,18 @@ window.addEventListener('load', () => {
     const sumResult = document.getElementById('sum-result');
     const sumAnswer = document.getElementById('sum-answer');
     const sumHidden = document.getElementById('sum-result-hidden');
-    
-    if (sumBtn) {
+
+    // Substract numbers
+    const subOne = document.getElementById('sub-one');
+    subOne.value = Math.floor(Math.random() * 9999) + 1;
+    const subTwo = document.getElementById('sub-two');
+    subTwo.value = Math.floor(Math.random() * 99) + 1;
+    const subBtn = document.getElementById('sub-button');
+    const subResult = document.getElementById('substr-result');
+    const subAnswer = document.getElementById('substr-answer');
+    const subHidden = document.getElementById('sub-result-hidden');
+
+    if (subBtn) {
         sumBtn.addEventListener('click', (e) => {
             e.preventDefault();
 
@@ -53,6 +64,57 @@ window.addEventListener('load', () => {
                 sumHidden.textContent = message;
                 setTimeout(() => {
                     sumHidden.style.display = "none";
+                }, 5000);
+                return;
+            }
+        });
+    }
+
+    if (subBtn) {
+        subBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            function sumNumbers() {
+                const result = Number(subOne.value) - Number(subTwo.value);
+                const answer = Number(subResult.value);
+
+                if (isNaN(subResult.value) || subResult.value === '') {
+                    return notify(`Подал си неправилни или липсващи данни! Въведи цифри!`);
+                }
+
+                subAnswer.disabled = false;
+                if (result === answer) {
+                    subHidden.style.display = "block";
+                    subAnswer.value = result.toString();
+                    subHidden.textContent = `Твоят отговор ${answer} е верен! Браво!`;
+                    setTimeout(() => {
+                        subOne.value = '';
+                        subTwo.value = '';
+                        subResult.value = '';
+                        subAnswer.value = '';
+                    }, 3000);
+                    setTimeout(() => {
+                        subHidden.style.display = "none";
+                        window.location.reload();
+                    }, 5000);
+                } else {
+                    subHidden.style.display = "block";
+                    subHidden.textContent = `Твоят отговор ${answer} е грешен! Опитай отново!`;
+                    subResult.value = '';
+                    subAnswer.value = '';
+                    setTimeout(() => {
+                        subHidden.style.display = "none";
+                    }, 7000);
+                }
+            }
+
+            sumNumbers();
+
+            function notify(message) {
+                subHidden.style.display = "block";
+                subHidden.textContent = message;
+                setTimeout(() => {
+                    subHidden.style.display = "none";
                 }, 5000);
                 return;
             }
